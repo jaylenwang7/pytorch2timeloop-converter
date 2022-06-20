2ff38f9059ef4c26d271e8612fd2cef07a059358
Jaylen Wang <jaylenwang@college.harvard.edu>
2022-06-20 10:01:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as fh:
+   requirements = fh.readlines()
+
 setup(name='pytorch2timeloop',
-        version='0.2',
-        url='https://github.com/Accelergy-Project/pytorch2timeloop-converter',
-        license='MIT',
-        install_requires=["torch==1.7", "torchvision==0.8", "numpy==1.19", "pyyaml==5.3"],
-        python_requires='>=3.6',
-        include_package_data=True,
-        packages=find_packages())
+      version='0.2',
+      url='https://github.com/Accelergy-Project/pytorch2timeloop-converter',
+      license='MIT',
+      install_requires=[req for req in requirements if req[:2] != "# "],
+      python_requires='>=3.6',
+      include_package_data=True,
+      packages=find_packages())
